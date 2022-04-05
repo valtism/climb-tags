68828d5b1ce3bb62f2f19478230b3fd5a66d5a44
Dan Wood <daniel4wood@icloud.com>
2022-04-05 12:22:40 +1000

    Change local dev port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import eslint from "@nabla/vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    port: 3663,
+  },
 });
