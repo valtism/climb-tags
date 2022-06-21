0fd3ce194be7410767f0f4ec0551b6fcbd51a525
Dan Wood <daniel4wood@icloud.com>
2022-06-21 15:37:59 +1000

    Adjust feedback form

--- components/FeedbackForm.tsx
+++ components/FeedbackForm.tsx
@@ -12,8 +12,12 @@ export function FeedbackForm() {
         </div>
       </div>
       <div className="col-span-2 mt-0">
-        {/* @ts-expect-error Using custom netlify attribute for form control */}
-        <form action="/success" method="POST" data-netlify="true" netlify>
+        <form
+          name="feedback"
+          action="/success"
+          method="POST"
+          data-netlify="true"
+        >
           <div className="overflow-hidden rounded-md shadow">
             <div className="space-y-6 bg-white p-6">
               <div>
