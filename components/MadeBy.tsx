0cc7af9ca493c22f9fc3fa72fbe23a13eb481c4c
Dan Wood <daniel4wood@icloud.com>
2022-06-21 15:34:12 +1000

    Add feedback page

--- components/MadeBy.tsx
+++ components/MadeBy.tsx
@@ -1,7 +1,7 @@
 export function MadeBy() {
   return (
-    <div className="flex justify-end space-x-1 p-2">
-      <span className="text-gray-500">Made by</span>
+    <div className="flex space-x-1 p-2">
+      <span className="text-gray-600">Made by</span>
       <a
         href="https://github.com/valtism/climb-tags"
         className="bg-gradient-to-r from-pink-400 via-purple-500 to-pink-400 bg-clip-text text-transparent drop-shadow filter"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Creator from "../components/Creator";
-import { FeedbackForm } from "../components/FeedbackForm";
 import { MadeBy } from "../components/MadeBy";
 
 const Home: NextPage = () => {
@@ -11,18 +11,16 @@ const Home: NextPage = () => {
         <title>Climb Tag Creator</title>
       </Head>
       <div className="flex min-h-screen flex-col font-Ubuntu selection:bg-rose-200">
+        <header className="flex items-center justify-between px-6 bg-gray-100 print:hidden mb-10">
+          <MadeBy />
+          <Link href={"/contact"}>
+            <a className="text-gray-600 hover:text-gray-900">Contact me</a>
+          </Link>
+        </header>
         <div className="mx-auto space-y-14 pt-6">
           <Creator />
           <Creator />
         </div>
-        <footer className="bg-gray-100 print:hidden mt-10">
-          <div className="mx-auto max-w-4xl py-10">
-            <FeedbackForm />
-          </div>
-          <div className="mx-4">
-            <MadeBy />
-          </div>
-        </footer>
       </div>
     </>
   );
